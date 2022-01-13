2ad6245adcbfadc5ff488c00d6e48b23b962bdb8
Mike Walchak <mfwalchak@gmail.com>
2022-01-13 11:23:41 -0700

    add Procfile and single server scripting

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -22,7 +22,7 @@ app.use("/reservations", reservationsRouter);
 app.use(express.static(path.join(__dirname, '../../front-end/build')))
 // AFTER defining routes: Anything that doesn't match what's above, send back index.html; (the beginning slash ('/') in the string is important!)
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + "../../front-end/build", "index.html"));
+  res.sendFile(path.join(__dirname + "../../front-end/build/index.html"));
 })
 
 
