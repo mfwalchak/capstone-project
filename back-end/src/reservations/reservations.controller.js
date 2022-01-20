e93e2a44f503adf511c70c20f64c236d0e3ecdbc
Mike Walchak <mfwalchak@gmail.com>
2022-01-20 13:45:28 -0700

    passing us-02 tests

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -12,21 +12,57 @@ const timeFormat = /\d\d:\d\d/;
 function isValidDateFormat(req, res, next){
   let { data = {} } = req.body;
   let date = data.reservation_date;
-  console.log("validator:", date, "date.test", !/\d\d\d\d-\d\d-\d\d/.test(date));
   try{
     if (!/\d\d\d\d-\d\d-\d\d/.test(date)){
         const error = new Error("reservation_date");
         error.status = 400;
-        //throw error;
     } next();
   } catch(error) {
     next(error);
   }
 }
 
+function isNotATuesday(req, res, next){
+  let { data = {} } = req.body;
+  let date = data.reservation_date;
+  let day = new Date(`${data.reservation_date} ${data.reservation_time}`)
+  console.log("isNotATuesdayValidator")
+  try{
+    if (day.getDay() === 2) {
+      const error = new Error(`${date}`);
+      error.message="closed";
+      error.status = 400;
+      throw error;
+    } next();
+  } catch(error) {
+    next(error);
+  }
+}
+
+function reservationNotInPast(req, res, next){
+  console.log("reservationNotInPastValidator")
+  let { data = {} } = req.body;
+  let date = data.reservation_date;
+  let day = new Date(`${data.reservation_date} ${data.reservation_time}`)
+
+  try {
+    console.log("reservationPastValidation")
+    if (day < new Date()) {
+      //console.log("Inside the NotInPast Condition!")//check if date is in the past
+      const error = new Error(`${date}`);
+      error.message="future";
+      error.status = 400;
+      throw error;
+    } next();
+  } catch(error) {
+    next(error);
+  }
+} 
+
 function isValidTimeFormat(req, res, next){
   let { data = {} } = req.body;
   let time = data.reservation_time;
+  //console.log("time", time);
   try{
     if (!/\d\d:\d\d/.test(time)){
         const error = new Error("reservation_time");
@@ -67,5 +103,5 @@ async function create(req, res, next) {
 
 module.exports = {
   list: asyncErrorBoundary(list),
-  post: [hasRequiredProperties, isValidDateFormat, isValidTimeFormat, isPartyValid, asyncErrorBoundary(create)],
+  post: [hasRequiredProperties, isValidDateFormat, isNotATuesday, reservationNotInPast, isValidTimeFormat, isPartyValid, asyncErrorBoundary(create)],
 };
